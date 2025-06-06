cb18030dd94a51b6489cb398eebafc3625bf383b
M Mostagir Bhuiyan <m.mostagir.bhuiyan.th@dartmouth.edu>
2025-06-06 11:56:53 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
  
 export default defineConfig({
   plugins: [react()],
-  base: '/reit-fund-model/', // Updated with actual repository name
+  base: '/multi-fund-model/', // Updated with actual repository name
 }); 
